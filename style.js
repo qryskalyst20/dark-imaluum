ec14fd6c9f162a915d292ec4e5e69fe39ce39ebf
qryskalyst20 <quddusnuriman@gmail.com>
2023-10-05 00:18:12 +0800

    :bug: fix: More error handling

--- style.js
+++ style.js
@@ -10,17 +10,22 @@ const parentItem = document.querySelector(
   "section.content div.row div.col-md-6:nth-child(2) .box.box-solid.box-shadow .box-body"
 );
 
-parentItem.style.display = "flex";
-parentItem.style.justifyContent = "center";
-parentItem.style.alignItems = "center";
+if (parentItem) {
+  parentItem.style.display = "flex";
+  parentItem.style.justifyContent = "center";
+  parentItem.style.alignItems = "center";
+}
 
 const newItem = document.createElement("div.newItem");
-newItem.innerHTML = ` 
+
+if (newItem) {
+  newItem.innerHTML = ` 
   <a href="https://github.com/qryskalyst20/dark-imaluum" target="_blank" style="color: white; font-weight: bold; background-color: green; padding: 20px; width: fit-content; border-radius: 10px;">
     Check out the source code on Github!
   </a>`;
 
-parentItem.innerHTML = newItem.innerHTML;
+  parentItem.innerHTML = newItem.innerHTML;
+}
 
 const image = document.querySelector("a.btn.bg-white img");
 
